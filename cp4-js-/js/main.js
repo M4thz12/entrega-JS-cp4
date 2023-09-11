56ec7c193fea6b0c58ee5ee4d29cc6f150528097
m4thz12 <rm98832@fiap.com.br>
2023-09-11 15:05:24 -0300

    commit

--- cp4-js-/js/main.js
+++ cp4-js-/js/main.js
@@ -1,19 +1,33 @@
+let salariosArray = [1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300, 2400, 2500];
 
-const arraySalarios = [
-    650, 1000, 1005, 3000, 2050, 1550, 8600, 2480, 1945, 4030
-];
-arraySalariosAtualizado = arraySalarios.map(salario =>{
+//Ex 1:
+//Essa função está criando um novo array com as condições determinadas de aumentos
+const salariosAumentados = salariosArray.map(salario => {
+    // aumento de 15% para salários até 2000
     if (salario <= 2000) {
-        return salario * 1.15; // Aumento de 15% para salários abaixo de 2000
-    } else {
-        return salario * 1.1;  // Aumento de 10% para salários acima de 2000
-      }
-    })
-    console.log(arraySalariosAtualizado)
+        return salario * 1.15;
+    } 
+    // aumento de 10% para salários acima de 2000
+    else {
+        return salario * 1.10;
+    }
+});
+//Print do Resultado dos salarios com os aumentos determidados (15% até 2000, 10% mais de 2000)
+console.log("Salários com o aumento")
+console.log(salariosAumentados);
 
-const salariosAcimaDe2500 = arraySalariosAtualizado.filter(salario => salario >= 2500)
-console.log(salariosAcimaDe2500)
 
+//Ex 2:
+//Essa função tá pegando os resultados do Ex 1 e filtrando somente os salarios maiores q 2500
+let maiorDoisQuinhentos = salariosAumentados.filter(salario=> salario> 2500);
 
-const somaSalariosAcimaDe2500 = salariosAcimaDe2500.reduce((somaTotal, salario)=> somaTotal + salario)
-console.log(somaSalariosAcimaDe2500)
+//Print do Array dos salarios maiores q 2500
+console.log("Salários maiores que 2500")
+console.log(maiorDoisQuinhentos);
+
+//Ex 3:
+//Nessa função foram atribuidos parametros que servem para somar os salarios do Ex 2
+let salarioSoma = maiorDoisQuinhentos.reduce((salario, soma)=> salario + soma);
+
+//Print da soma dos salarios da array do ex 2
+console.log(`A soma dos salarios maiores que 2500 é ${salarioSoma}`);
